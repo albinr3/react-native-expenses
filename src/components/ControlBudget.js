ab1ac8cfd18d75fce45dea0ecfb337193d52dcd8
Albin Rodriguez 2 <albinmrodriguez2@gmail.com>
2022-07-15 15:55:48 -0400

    adding icons and pdf creator

--- src/components/ControlBudget.js
+++ src/components/ControlBudget.js
@@ -1,7 +1,9 @@
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, Text, Platform, PermissionsAndroid, Pressable,ToastAndroid, StyleSheet} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import globalStyles from '../styles';
 import CircularProgress from 'react-native-circular-progress-indicator';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+import { Dirs, FileSystem } from 'react-native-file-access';
 
 const ControlBudget = ({budget, spents, resetApp}) => {
 
@@ -28,6 +30,57 @@ const ControlBudget = ({budget, spents, resetApp}) => {
     setAvailable(availableTotal);
   }, [spents])
 
+  const createPdf = async () => {
+    let options = {
+      html: '<h1>Albin</h1>',
+      fileName: 'test',
+      directory: 'Documents',
+      base64: true
+    };
+    try {
+      let file = await RNHTMLtoPDF.convert(options)
+      // console.log(file.filePath);
+      alert(file.filePath);
+
+      const fileName = 'test.pdf'; //whatever you want to call your file
+      
+      const filePath = `${Dirs.DocumentDir}/${fileName}`;
+      const base64Data = file.base64; //our base64 encode file which done by RNHTMLtoPDF;
+
+      const permissionWriteExternalStorage = async () => {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+        )
+        return granted === PermissionsAndroid.RESULTS.GRANTED
+    }
+
+      if (Platform.OS === 'android') {
+        const permissionGranted = await permissionWriteExternalStorage();
+        if (permissionGranted) {
+           await FileSystem.writeFile(filePath, base64Data, 'base64');
+    
+           if (!FileSystem.exists(filePath)) return;// check to see if our filePath was created
+    
+           await FileSystem.cpExternal(filePath, fileName,'downloads');// copies our file to the downloads folder/directory
+           // file should now be visible in the downloads folder
+           ToastAndroid.show("One File Downloaded", ToastAndroid.SHORT);
+        }
+    
+        return;
+      }
+
+      if (Platform.OS === 'ios') {
+      // IOS version
+      await FileSystem.writeFile(filePath, base64Data, 'base64');
+      Alert.alert('', 'One File Downloaded');
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
   return (
     <View style={s.container}>
       <View style={s.centerImage}>
@@ -48,6 +101,10 @@ const ControlBudget = ({budget, spents, resetApp}) => {
 
       <Pressable onLongPress={resetApp} style={s.btn}>
         <Text style={s.textBtn}>Reset App</Text>
+      </Pressable>
+
+      <Pressable onPress={createPdf} style={s.btn}>
+        <Text style={s.textBtn}>PDF</Text>
       </Pressable>
 
       <View style={s.textContainer}>
